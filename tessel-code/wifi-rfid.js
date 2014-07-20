3b668c01f6b9e790fe13d8b1e7808ed99b200811
dhruvarora <dhruvarora@berkeley.edu>
2014-07-19 17:42:29 -0700

    Reading objects successfully

--- tessel-code/wifi-rfid.js
+++ tessel-code/wifi-rfid.js
@@ -4,18 +4,21 @@ var https = require("https");
 var rfidlib = require('rfid-pn532');
 var rfid = rfidlib.use(tessel.port['A']);
 
-var dataTable = '';
+var masterpackDBString = '';
+var masterpackDB;
 
-var masterpack = https.get('https://smartpack.firebaseio.com/masterpack.json', function(res) {
+var req = https.get('https://smartpack.firebaseio.com/masterpack.json', function(res) {
   console.log("Status Code:", res.statusCode);
   res.on('data', function(d) {
-    dataTable += d;
+    masterpackDBString += d;
   });
   res.on('end', function() {
     console.log('Finished Reading Firebase');
+    rfidRead();
   });
+});
 
-}).on('error', function(e) {
+req.on('error', function(e) {
   console.error(e);
 });
 
@@ -26,19 +29,22 @@ var rfidRead = function() {
     rfid.on('data', function(card) {
       var cardIDString = card.uid.toString('hex');
       console.log(cardIDString);
-      console.log(masterpack);
-      console.log('You added a ' + masterpack[cardIDString]);
+      // console.log("DB STRING:", masterpackDBString);
+      if (masterpackDBString) {
+        masterpackDB = JSON.parse(masterpackDBString);
+        console.log(masterpackDB[cardIDString[1]]);
+        if (masterpackDB[cardIDString][1]) {
+          console.log('You added your ' + masterpackDB[cardIDString][0]);
+        } else {
+          console.log('You removed your ' + masterpackDB[cardIDString][0]);
+        }
+      } else {
+        console.log('MasterpackDBString came back empty!');
+      }
     });
   });
 
   rfid.on('error', function (err) {
     console.error(err);
-  });
-}();
-
-var createObjectFirebase = function(cardIDString) {
-  var smartpackObjectDB = new Firebase('https://smartpack.firebaseio.com/masterpack/' + cardIDString);
-  dataRef.on('value', function(snapshot) {
-    console.log('You added your ' + snapshot.val());
   });
 };
